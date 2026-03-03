999466ac7501eb5825a717ca2bfd296e141bf2e4
azizalmachzumi47 <azizalmachzumi21@gmail.com>
2026-03-03 13:23:06 +0700

    add postman collection, zip project, and update database

--- db/db_applaravel.sql
+++ db/db_applaravel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 03, 2026 at 07:02 AM
+-- Generation Time: Mar 03, 2026 at 07:10 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
